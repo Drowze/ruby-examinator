97c69eb16fae4d82b997206933d47e14babab8b5
Rafael Gibim <gibim6@gmail.com>
2015-12-12 18:04:58 +0000

    quick fixes

--- examinator.rb
+++ examinator.rb
@@ -1,27 +1,28 @@
-require_relative './in_out_parser.rb'
+require_relative './lib/in_out_parser.rb'
 extend InOutParser
 
 ## Converting line endings to unix:
-InOutParser.newlines_converter('./libs/inputs.txt')
-InOutParser.newlines_converter('./libs/outputs.txt')
+InOutParser.newlines_converter('./data/inputs.txt')
+InOutParser.newlines_converter('./data/outputs.txt')
 
 ## Getting each input and each output into an array
-inputs = InOutParser.parse_paragraphs('./inputs.txt')
-outputs = InOutParser.parse_paragraphs('./outputs.txt')
+inputs = InOutParser.parse_paragraphs('./data/inputs.txt')
+outputs = InOutParser.parse_paragraphs('./data/outputs.txt')
 
 # Useful for debugging.
 # InOutParser.cute_print(inputs, 0.5)
 # InOutParser.cute_print(outputs, 0.5)
 
 # Getting an array containing all the source files:
-source_files_directory = Dir.getwd + '/sources/'
+source_files_directory = Dir.getwd + '/data/sources/'
+p source_files_directory
 source_files = Dir.entries(source_files_directory).select { |a| a != '.' && a != '..' }
 
 # Executing them and comparing to the outputs:
 preserve_binaries = true
 preserve_outputs = true
 
-`mkdir temp`
+`mkdir -p temp`
 source_files.each do |source|
   puts "On file #{source} now..."
   counter = 0
@@ -33,21 +34,22 @@ source_files.each do |source|
     source_no_extension = source.split('.')[0]
 
     if preserve_binaries
-      `mkdir -p bin`
-      gcc_output = 'bin/' + source_no_extension + '.exe'
+      `mkdir -p results/bin`
+      gcc_output = 'results/bin/' + source_no_extension + '.exe'
     else
       gcc_output = 'temp/a.out'
     end
 
     if preserve_outputs
-      `mkdir -p outputs/#{source_no_extension}`
-      file_output = 'outputs/' + source_no_extension + "/#{counter += 1}.txt"
+      `mkdir -p results/outputs/#{source_no_extension}`
+      file_output = 'results/outputs/' + source_no_extension + "/#{counter += 1}.txt"
     else
       file_output = 'temp/' + source_no_extension
     end
 
-    `g++ sources/#{source} -w -o #{gcc_output}`
+    `g++ data/sources/#{source} -w -o #{gcc_output}`
     `./#{gcc_output} < temp/input_temp.txt > #{file_output}`
+    `rm -r temp`
   end
 end
 
